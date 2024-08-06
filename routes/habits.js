e08eadb903aed695ecef9ceb4a67ffb69428bf37
Maryam <ma.abde.contact@gmail.com>
2024-08-06 15:26:44 +0200

    update route for habits v1

--- routes/habits.js
+++ routes/habits.js
@@ -1,15 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
+const Task = require("../models/tasks");
 const { checkBody } = require("../functions/checkbody");
 
-// if (!checkBody(req.body, ['name', 'email', 'password'])) {
-//     res.json({ result: false, error: 'Missing or empty fields' });
-//     return;
-// }
-
-const Task = require("../models/tasks");
-
+//  Route post pour la creation d'une habitude
 router.post("/create", (req, res) => {
   // Verification de la validiter des infos envoyer par le body
   if (!checkBody(req.body, ["name", "number", "label"])) {
@@ -19,14 +14,12 @@ router.post("/create", (req, res) => {
 
   // Destructuration des infos envoyer par le body
   const {
-    // token,
     name,
     description,
     tags,
     dificulty,
     number,
     label,
-    toDo,
     isFavorite,
     urgent,
   } = req.body;
@@ -35,27 +28,60 @@ router.post("/create", (req, res) => {
     // Verification que l'habitude n'existe pas déja
     Task.findOne({
       name: { $regex: new RegExp(name, "i") },
-    })
-    .then((data) => {
+    }).then((data) => {
+      // Creation de l'habitude dans la base de donnée 
       if (!data) {
         const newTask = new Task({
           name,
           description,
           tags,
           dificulty,
-          number,
-          label,
-          toDo,
+          repetition : {
+            number,
+            label
+          },
           isFavorite,
           urgent,
         });
 
         newTask.save().then((data) => {
-          res.json({ data });
+          res.json({ result: true, data });
         });
       } else {
         res.json({ result: false, errorMsg: "Cette habitude existe déja." });
       }
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+router.post("/modify", (req, res) => {
+  // Verification de la validiter des infos envoyer par le body
+  if (!checkBody(req.body, ["name", "number", "label"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // Destructuration des infos envoyer par le body
+  const {
+    name,
+    newName,
+    description,
+    tags,
+    dificulty,
+    number,
+    label,
+    isFavorite,
+  } = req.body;
+  try {
+    Task.updateOne(
+      { name: { $regex: new RegExp(name, "i") } },
+      { name: newName, description, tags, dificulty, number, label, isFavorite }
+    ).then(() => {
+      Task.find().then((data) => {
+        res.json({ result: true, data });
+      });
     });
   } catch (error) {
     alert(error.message);
